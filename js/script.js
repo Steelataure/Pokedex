74ff8f1b3ba942442aac0dae0cf0b2dcbdd2f9cf
Steelataure <alexandre0312@orange.fr>
2023-03-28 14:51:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-    
-    $(document).ready(function () {
-        $('#example').DataTable();
-    });
-    
+$('#example').dataTable({
+    aLengthMenu: [
+        [25, 50, 100, 200, -1],
+        [25, 50, 100, 200, "All"]
+    ],
+    iDisplayLength: -1
+});
