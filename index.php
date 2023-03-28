a0207a41d128e4b08988a196d1dc804b91da9f69
Steelataure <alexandre0312@orange.fr>
2023-03-28 11:44:31 +0200

    update

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=pokemon_api", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+require_once "connect_db.php";
 
 
 $sqlQuery = 'SELECT * FROM pokemon';
@@ -21,14 +10,10 @@ $pokemon = $recipesStatement->fetchAll();
 
 $clients_max = 10;
 foreach ($pokemon as $pokemons) {
-?>
-    <?php 
+
     $id = $pokemons['id'];
     $name = $pokemons['identifier'];
-    ?>
     
-
-<?php
     echo $id;
     echo $name;
 }
