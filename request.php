1f96be3fbdb0b1d76b1a2067a2ab67fb08db9be0
Steelataure <alexandre0312@orange.fr>
2023-03-28 14:43:01 +0200

    update

--- request.php
+++ request.php
@@ -1,11 +1,7 @@
 <?php
 require_once "connect_db.php";
 
-
 $sqlQuery = 'SELECT * FROM pokemon';
 $recipesStatement = $conn->prepare($sqlQuery);
 $recipesStatement->execute();
 $pokemon = $recipesStatement->fetchAll();
-
-
-
