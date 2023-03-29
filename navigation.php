da5affa3921d2e746578eeeb802ed72d21dd15b4
Steelataure <alexandre0312@orange.fr>
2023-03-29 13:30:38 +0200

    update

--- navigation.php
+++ navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="#">Pokedex</a>
+	<a class="navbar-brand" href="index.php">Pokedex</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" arial-controls="navbarSupportedContent" arial-expanded="false" arial-label="toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
