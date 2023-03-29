096437e27270b6c0b953ae364a7e565f7620aac3
ChatLucas <108232102+ChatLucas@users.noreply.github.com>
2023-03-29 13:53:39 +0200

    Fix

--- detail.php
+++ detail.php
@@ -71,71 +71,25 @@ endif;
 <div class="evol">
     Famille d'évolution de <?= $response["name"]['fr']; ?><br>
     <?php
-
-    //SI A 2 EVOLUTIONS
-    if ($response["evolution"]['next'][0]['name'] && $response["evolution"]['next'][1]['name'])
-    {
-    ?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
+	
+	foreach ($response["evolution"]['pre'] as $evolution)
+	{
+		?>
+		<img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $evolution['pokedexId'] ?>/regular.png">
+		<?php
+		echo $evolution['name']  . "<br/>";
+	}
+	
+	?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
     echo $response["name"]['fr'] . "<br/>";
-
-    ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] + 1 ?>/regular.png"> <?php
-    echo $response["evolution"]['next'][0]['name']  . "<br/>";
-    
-    ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] + 2 ?>/regular.png"> <?php
-    echo $response["evolution"]['next'][1]['name'] . "<br/>";
-
-    }
-    //SI UNE SEULE EVOLUTION
-    elseif ($response["evolution"]['next'][0]['name'])
-    {
-    if ($response["evolution"]['pre'][0]['name']){ //SI UNE ARRIERE ET UN EVOLUTION
-
-        ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] - 1 ?>/regular.png"> <?php
-        echo $response["evolution"]['pre'][0]['name'] . "<br/>"; 
-        
-        ?> <img src=" <?= $response['sprites']['regular'] ?>"> <?php
-        echo $response["name"]['fr'] . "<br/>";
-        
-        ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] + 1 ?>/regular.png"> <?php
-        echo $response["evolution"]['next'][0]['name'] . "<br/>";
-
-    }
-    else // SI PAS ARRIERE ET UNE EVOLUTION
-    {
-        ?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
-        echo $response["name"]['fr'] . "<br/>";
-
-        ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] + 1 ?>/regular.png"> <?php
-        echo $response["evolution"]['next'][0]['name'] . "<br/>";
-    }
-    }
-    //SI 2 ARRIERES
-    elseif ($response["evolution"]['pre'][0]['name'] && $response["evolution"]['pre'][1]['name'])
-    {
-    ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] - 2 ?>/regular.png"> <?php
-    echo $response["evolution"]['pre'][0]['name'] . "<br/>";
-    
-    ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] - 1 ?>/regular.png"> <?php
-    echo $response["evolution"]['pre'][1]['name'] . "<br/>";
-
-    ?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
-    echo $response["name"]['fr'] . "<br/>";
-    }
-    //SI UN SEUL ARRIERE
-    elseif ($response["evolution"]['pre'][0]['name'])
-    {
-    ?> <img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $_GET['id'] - 1 ?>/regular.png"> <?php
-    echo $response["evolution"]['pre'][0]['name'] . "<br/>";
-
-    ?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
-    echo $response["name"]['fr'] . "<br/>";
-
-    }
-    else //SI 0 FAMILLES
-    {
-    ?> <img src=" <?= $response['sprites']['regular'] ?>"><?php
-    echo $response["name"]['fr'] . "<br/>";
-    }
+	
+	foreach ($response["evolution"]['next'] as $evolution)
+	{
+		?>
+		<img src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/<?= $evolution['pokedexId'] ?>/regular.png">
+		<?php
+		echo $evolution['name']  . "<br/>";
+	}
     ?>
 </div>
 </body>
