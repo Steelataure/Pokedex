6a7d6f447f978ceffebd0a878025e5b56a071672
ChatLucas <108232102+ChatLucas@users.noreply.github.com>
2023-03-29 13:23:36 +0200

    add tableau_type

--- table_type.php
+++ table_type.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
 <html>
+<?php
+require_once "header.php";
+require_once "navigation.php";
+?>
 
+<body>
+	<img src="images/tableau_types.png" class="img-fluid" style="width: 100%; height: auto;">
+</body>
 </html>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
