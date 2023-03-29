be2d5fa46e469a7ddaa50e57006139dc9343e585
Pata32 <a.chambrin32@gmail.com>
2023-03-29 11:06:52 +0200

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="#">Navigation</a>
+	<a class="navbar-brand" href="#">Pokedex</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" arial-controls="navbarSupportedContent" arial-expanded="false" arial-label="toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
@@ -7,7 +7,7 @@
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item">
-				<a class="nav-link" href="#">Home</a>
+				<a class="nav-link" href="index.php">Accueil</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="table_type.php">Table des types</a>
